f352c6d8832e0d8d95140ccbe068d6e8e665fc46
vignouds <sebastien.vignoud@gmail.com>
2018-02-20 14:47:17 +0100

    Création de index.html script.js et style.css. Création de la map grace à l'API de google

---
+++ script.js
@@ -0,0 +1,7 @@
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 45.765591, lng: 4.833026},
+          zoom: 13
+        });
+      }
