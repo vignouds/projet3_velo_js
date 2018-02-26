086aeca1d70c423ca1c0eb41b508904e3701b3ac
vignouds <sebastien.vignoud@gmail.com>
2018-02-26 22:23:18 +0100

    requête ajax API JCDeceaux

--- script.js
+++ script.js
@@ -25,3 +25,36 @@ var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 13
 });
 }
+
+/* Récupération des données de l'API JCDecaux */
+ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=06d1abf31ef9614eebf8a70d0ab679b30d3cf7ed", function (reponse) {
+	// Transforme la réponse en un tableau d'articles
+    var velo = JSON.parse(reponse);
+
+    // Récupération de certains résultats
+
+/*    var temperature = meteo.current_observation.temp_c;
+
+    var humidite = meteo.current_observation.relative_humidity;
+
+    var imageUrl = meteo.current_observation.icon_url;*/
+
+    // Affichage des résultats
+
+/*    var conditionsElt = document.createElement("div");
+
+    conditionsElt.textContent = "Il fait actuellement " + temperature +
+
+        "°C et l'humidité est de " + humidite;
+
+    var imageElt = document.createElement("img");
+
+    imageElt.src = imageUrl;
+
+    var meteoElt = document.getElementById("meteo");
+
+    meteoElt.appendChild(conditionsElt);
+
+    meteoElt.appendChild(imageElt);*/
+
+});
