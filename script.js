84f45ab183d79477ad9d88f809bb0a6a5d719d0e
vignouds <sebastien.vignoud@gmail.com>
2018-02-27 13:34:04 +0100

    marker google map, amélioration requete ajax

--- script.js
+++ script.js
@@ -20,16 +20,46 @@ function showSlides(n) {
 
 /*Google map*/
 function initMap() {
+var lyon = {lat: 45.765591, lng: 4.833026};
 var map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 45.765591, lng: 4.833026},
+  center: lyon,
   zoom: 13
 });
+var marker = new google.maps.Marker({
+      position: lyon,
+      map: map
+  });
+}
+
+
+// Exécute un appel AJAX GET
+// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
+function ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            // Appelle la fonction callback en lui passant la réponse de la requête
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
 }
 
 /* Récupération des données de l'API JCDecaux */
 ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=06d1abf31ef9614eebf8a70d0ab679b30d3cf7ed", function (reponse) {
 	// Transforme la réponse en un tableau d'articles
-    var velo = JSON.parse(reponse);
+    var velos = JSON.parse(reponse);
+    velos.forEach(function(velo) {
+      console.log(velo.name);
+      console.log(velo.position.lat);
+      console.log(velo.position.lng);
+    });
 
     // Récupération de certains résultats
 
