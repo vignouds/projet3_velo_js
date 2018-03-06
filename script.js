7dd9f926cb1462df61233fe218f92ea07d60eba4
vignouds <sebastien.vignoud@gmail.com>
2018-03-06 14:50:46 +0100

    creation classe marqueur et tableau, bug map

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-
-/*Slider*/
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
- 	showSlides(slideIndex += n);	
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slider");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-}
-
-/*Google map*/
-function initMap() {
-var lyon = {lat: 45.765591, lng: 4.833026};
-var map = new google.maps.Map(document.getElementById('map'), {
-  center: lyon,
-  zoom: 13
-});
-var marker = new google.maps.Marker({
-      position: lyon,
-      map: map
-  });
-}
-
-
 // Exécute un appel AJAX GET
 // Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
 function ajaxGet(url, callback) {
@@ -51,40 +17,43 @@ function ajaxGet(url, callback) {
     req.send(null);
 }
 
+//Classe Marqueur
+var Marqueur = {
+  initMarqueur: function(name, lattitude, longitude){
+    this.name = name;
+    this.lattitude = lattitude;
+    this.longitude = longitude;
+  }
+};
+
+var marqueurs = [];
+
 /* Récupération des données de l'API JCDecaux */
 ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=06d1abf31ef9614eebf8a70d0ab679b30d3cf7ed", function (reponse) {
 	// Transforme la réponse en un tableau d'articles
-    var velos = JSON.parse(reponse);
-    velos.forEach(function(velo) {
-      console.log(velo.name);
-      console.log(velo.position.lat);
-      console.log(velo.position.lng);
+    var stations = JSON.parse(reponse);
+    stations.forEach(function(station) {
+      var marqueur = Object.create(Marqueur);
+      marqueur.initMarqueur(station.name, station.position.lat, station.position.lng);
+      marqueurs.push(marqueur);
     });
-
-    // Récupération de certains résultats
-
-/*    var temperature = meteo.current_observation.temp_c;
-
-    var humidite = meteo.current_observation.relative_humidity;
-
-    var imageUrl = meteo.current_observation.icon_url;*/
-
-    // Affichage des résultats
-
-/*    var conditionsElt = document.createElement("div");
-
-    conditionsElt.textContent = "Il fait actuellement " + temperature +
-
-        "°C et l'humidité est de " + humidite;
-
-    var imageElt = document.createElement("img");
-
-    imageElt.src = imageUrl;
-
-    var meteoElt = document.getElementById("meteo");
-
-    meteoElt.appendChild(conditionsElt);
-
-    meteoElt.appendChild(imageElt);*/
-
 });
+
+/*Google map*/
+function initMap() {
+var lyon = {lat: 45.765591, lng: 4.833026};
+var map = new google.maps.Map(document.getElementById('map'), {
+  center: lyon,
+  zoom: 13
+});
+var marker = new google.maps.Marker({
+    position: lyon,
+    map: 'map'
+});
+/*marqueurs.forEach(function (marq) {
+  var marker = new google.maps.Marker({
+      position: {lat: marqueur.lattitude, lng: marqueur.longitude},
+      map: 'map'
+  });
+});*/
+}
